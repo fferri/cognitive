2f2202577a7b5a6bb6a75481400da9d603b5d850
Federico Ferri <federico.ferri.it@gmail.com>
2013-10-15 02:14:08 -0700

    use ros params; add namespace;

--- scripts/mapper.py
+++ scripts/mapper.py
@@ -10,18 +10,23 @@ from memory.lib import *
 
 def mapper():
     rospy.init_node('memory_mapper')
-    topic_name = sys.argv[1]
-    topic_type = sys.argv[2]
-    timeout = int(sys.argv[3])
-    term_functor = sys.argv[4]
-    term_args_expr = sys.argv[5:]
+    namespace = rospy.get_param('~memory_namespace', '/memory')
+    topic_name = rospy.get_param('~topic_name')
+    topic_type = rospy.get_param('~topic_type')
+    timeout = int(rospy.get_param('~timeout', '0'))
+    term_functor = rospy.get_param('~term/functor')
+    term_args_expr = []
+    i = 0
+    while rospy.has_param('~term/args/%d' % i):
+        term_args_expr.append(rospy.get_param('~term/args/%d' % i))
+        i += 1
 
     topic_class = roslib.message.get_message_class(topic_type)
     if not topic_class:
         print('Message %s not found. (are messages built?)' % topic_type)
         return
 
-    mm = MemoryTopicMapper(topic_name, topic_class, timeout, term_functor, term_args_expr)
+    mm = MemoryTopicMapper(topic_name, topic_class, term_functor, term_args_expr, timeout=timeout, ns=namespace)
 
     r = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
