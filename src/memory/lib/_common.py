105301c07646e66ac09cf8d9b908b7b72508f6c5
Federico Ferri <federico.ferri.it@gmail.com>
2013-10-15 02:16:11 -0700

    add get_terminal_size fn to print_terms_table

--- src/memory/lib/_common.py
+++ src/memory/lib/_common.py
@@ -121,8 +121,39 @@ def term_is_ground(t):
             return False
     return True
 
+def get_terminal_size():
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+
+        ### Use get(key[, default]) instead of a try/catch
+        #try:
+        #    cr = (env['LINES'], env['COLUMNS'])
+        #except:
+        #    cr = (25, 80)
+    return int(cr[1]), int(cr[0])
+
 def print_terms_table(entries):
-    colw = [8, -50, -12]
+    (width, height) = get_terminal_size()    
+    #colw = [8, -50, -12]
+    colw = [8, -(width - 10 - 12 - 8), -12]
     fmt = '| ' + ' | '.join("%%%ds" % w for w in colw) + ' |'
     hline = '+' + '+'.join('-' * (abs(w) + 2) for w in colw) + '+'
     print hline
