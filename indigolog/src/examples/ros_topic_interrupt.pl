0c4547e16490b6223e9cf5369b9b2c2856defc60
Federico Ferri <federico.ferri.it@gmail.com>
2013-12-11 11:20:42 +0100

    add some examples for ros topics

---
+++ indigolog/src/examples/ros_topic_interrupt.pl
@@ -0,0 +1,28 @@
+% declaring a fluent with the name of the topic makes automatically update it
+% when subscribing to that topic:
+prim_fluent('/cmd_vel').
+initially('/cmd_vel', nil).
+
+% utility action to clear the value of the fluent:
+prim_action(clear(F)) :- prim_fluent(F).
+poss(clear(F), neg(F=nil)) :- prim_fluent(F).
+causes_val(clear(F), F, nil, true) :- prim_fluent(F).
+execute(clear(F), _) :- prim_fluent(F).
+
+proc(control,
+    [
+        say('control started'),
+
+        say('subscribing to /cmd_vel topic...'),
+        subscribe('/cmd_vel', 'geometry_msgs/Twist'),
+
+        say('entering interrupt block...'),
+        prioritized_interrupts([
+            interrupt('/cmd_vel'=nil, wait),
+            interrupt(neg('/cmd_vel'=nil), pi(msg, [?(msg='/cmd_vel'), say(msg), clear('/cmd_vel')]))
+        ]),
+
+        say('control is terminating')
+    ]
+).
+
---
+++ indigolog/src/examples/ros_topic_subscribe.pl
@@ -0,0 +1,25 @@
+% declaring a fluent with the name of the topic makes automatically update it
+% when subscribing to that topic:
+prim_fluent('/cmd_vel').
+initially('/cmd_vel', nil).
+
+proc(control,
+    [
+        say('control started'),
+
+        say('subscribing to /cmd_vel topic...'),
+        subscribe('/cmd_vel', 'geometry_msgs/Twist'),
+
+        say('waiting for some message on the /cmd_vel topic...'),
+        while('/cmd_vel'=nil, [wait]),
+
+        pi(msg, [
+            ?(msg='/cmd_vel'),
+            say(['received a message:', msg]),
+            pi(linx, [?(dict_get(msg, [linear,x], linx)), say(['linear.x value is: ', linx])])
+        ]),
+
+        say('control is terminating')
+    ]
+).
+
