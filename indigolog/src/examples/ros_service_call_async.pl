f31316484390cef5e09d940725815b4fc4985dda
Federico Ferri <federico.ferri.it@gmail.com>
2014-01-09 14:41:57 +0100

    minor fixes (topic names, error handling in service callback)

--- indigolog/src/examples/ros_service_call_async.pl
+++ indigolog/src/examples/ros_service_call_async.pl
@@ -1,17 +1,16 @@
 % declare a fluent with the name of the service to receive its response data:
-prim_fluent('/ask').
-initially('/ask', nil).
+prim_fluent('/mixed_initiative_gui/add').
+initially('/mixed_initiative_gui/add', nil).
 
 proc(control,
     [
-        say('calling /ask service asynchronously...'),
-        call_service_async('/ask', 'mixed_initiative_gui/Question', 'mixed_initiative_gui/Question'([message('Are you happy?'), buttons(['Yes','No'])]), 0),
+        say('calling service asynchronously...'),
+        call_service_async('/mixed_initiative_gui/add', 'mixed_initiative_gui/Question', 'mixed_initiative_gui/Question'([message('Are you happy?'), buttons(['Yes','No'])]), 0),
 
-        while('/ask'=nil, [say(...),sleep(1)]),
-        pi(raw, [?(raw='/ask'), say(['raw answer: ',raw])]),
+        while('/mixed_initiative_gui/add'=nil, [say(...),sleep(1)]),
 
         pi(x, [
-            ?(x='/ask'),
+            ?(x='/mixed_initiative_gui/add'),
             pi(y, [
                 ?(dict_get(x,text_input,y)),
                 say(['y=',y]),
